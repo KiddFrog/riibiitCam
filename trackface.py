e545d63daa2ca5102cda0f05778a7854acfa42c0
domm <dommferger@gmail.com>
2023-11-14 17:37:43 -0500

    face

--- trackface.py
+++ trackface.py
@@ -1,24 +1,26 @@
 import cv2
-import libcamera
+import subprocess
+import numpy as np
+import sys
 
-# Initialize libcamera
-libcamera.start()
-
-# Open the first camera (you might need to adjust the camera index)
-camera = libcamera.Camera(0)
+# Open libcamera-hello subprocess
+cmd = ["libcamera-hello", "-t", "5000", "-s", "1280x720", "-f", "NV12"]
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Load the pre-trained face detector
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
-    # Capture a frame from the camera
-    frame = camera.capture()
+    # Read a frame from libcamera-hello stdout
+    raw_frame = process.stdout.read(1280 * 720 * 1.5)  # Assuming resolution is 1280x720 and format is NV12
+    if not raw_frame:
+        break
 
-    # Convert the frame to grayscale for face detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Convert the NV12 frame to BGR
+    frame = cv2.cvtColor(cv2.imdecode(np.frombuffer(raw_frame, dtype=np.uint8), cv2.IMREAD_COLOR), cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
 
     # Draw bounding boxes around the detected faces
     for (x, y, w, h) in faces:
@@ -31,6 +33,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release libcamera resources
-libcamera.stop()
+# Release libcamera-hello subprocess
+process.terminate()
 cv2.destroyAllWindows()
