cacb4179aaeb40d998437dda54339c694e1027c1
domm <dommferger@gmail.com>
2023-11-16 10:29:51 -0500

    New facial tracking

--- trackface.py
+++ trackface.py
@@ -7,25 +7,33 @@ import sys
 cmd = ["libcamera-hello", "-t", "5000", "-s", "1280x720", "-f", "NV12"]
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# Load the pre-trained face detector
-haarcascade_path = "/home/froggo/haarcascade_frontalface_default.xml"
-face_cascade = cv2.CascadeClassifier(haarcascade_path)
+# Load the SSD model for face detection
+model_path = "opencv_face_detector_uint8.pb"
+config_path = "opencv_face_detector.pbtxt"
+net = cv2.dnn.readNetFromTensorflow(model_path, config_path)
 
 while True:
     # Read a frame from libcamera-hello stdout
-    raw_frame = process.stdout.read(int(1280 * 720 * 1.5))  # Assuming resolution is 1280x720 and format is NV12
+    raw_frame = process.stdout.read(int(1280 * 720 * 1.5))
     if not raw_frame:
         break
 
     # Convert the NV12 frame to BGR
     frame = cv2.cvtColor(cv2.imdecode(np.frombuffer(raw_frame, dtype=np.uint8), cv2.IMREAD_COLOR), cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    # Use the SSD model for face detection
+    height, width = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False)
+    net.setInput(blob)
+    detections = net.forward()
 
     # Draw bounding boxes around the detected faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5:  # Adjust confidence threshold as needed
+            box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
+            (startX, startY, endX, endY) = box.astype("int")
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Face Detection', frame)
