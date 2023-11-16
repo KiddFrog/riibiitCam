7d3bcb76518aeab2b12d370674b96cffb3e40709
domm <dommferger@gmail.com>
2023-11-16 11:30:04 -0500

    Trying picamera

--- track_video.py
+++ track_video.py
@@ -1,28 +1,42 @@
 import cv2
+import picamera
+import picamera.array
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# To capture video from webcam. 
-cap = cv2.VideoCapture(libcamera-jpeg -o)
-# To use a video file as input 
-# cap = cv2.VideoCapture('filename.mp4')
+# Initialize the PiCamera
+with picamera.PiCamera() as camera:
+    # Set the resolution (adjust as needed)
+    camera.resolution = (640, 480)
 
-while True:
-    # Read the frame
-    _, img = cap.read()
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display
-    cv2.imshow('img', img)
-    # Stop if escape key is pressed
-    k = cv2.waitKey(30) & 0xff
-    if k==27:
-        break
-# Release the VideoCapture object
-cap.release()
+    # Use the picamera.array module for efficient capture to OpenCV format
+    with picamera.array.PiRGBArray(camera) as output:
+        while True:
+            # Capture a frame from the camera
+            camera.capture(output, 'bgr')
+            img = output.array
+
+            # Convert to grayscale
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Detect the faces
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+            # Draw the rectangle around each face
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            # Display the image
+            cv2.imshow('img', img)
+
+            # Clear the stream for the next frame
+            output.truncate(0)
+
+            # Stop if escape key is pressed
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:
+                break
+
+# Destroy all OpenCV windows
+cv2.destroyAllWindows()
