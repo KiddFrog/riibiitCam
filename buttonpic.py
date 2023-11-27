121ae2dfd3875f8c1240ede72e5e3c7ffd2fbd3f
domm <dommferger@gmail.com>
2023-11-27 16:01:15 -0500

    yes

--- buttonpic.py
+++ buttonpic.py
@@ -1,12 +1,20 @@
 import subprocess
 from gpiozero import Button
+from PIL import Image
 from signal import pause
 
 # Create a Button object for capturing photos
 button = Button(21)
 
+# Set the output directory for the photos
+OUTPUT_DIR = "/home/pi/Desktop/riibiit/PICTURES"
+
+# Set the dimensions for each camera image
+WIDTH = 2328
+HEIGHT = 1748
+
 # Set the initial value of the preview flag
-preview_flag = False
+preview_flag = True
 
 def toggle_preview():
     global preview_flag
@@ -18,10 +26,23 @@ def toggle_preview():
     else:
         # Stop the video streaming
         subprocess.run(["pkill", "-f", "libcamera-vid"])
-        print("Preview stopped. Capturing photo...")
+        print("Preview stopped.")
 
-# Assign the function to the button release event
+def capture_photo():
+    if preview_flag:
+        # Generate a unique filename based on the current date and time
+        filename = time.strftime("%Y%m%d-%H%M%S")
+
+        # Use libcamera-jpeg to capture a photo and save it to the output directory
+        subprocess.run(["libcamera-jpeg", "-o", f"{OUTPUT_DIR}/{filename}.jpg"])
+
+        print(f"Photo captured and saved as {filename}.jpg")
+    else:
+        print("Cannot capture photo without preview. Press and release the button to start the preview.")
+
+# Assign the functions to the button events
 button.when_released = toggle_preview
+button.when_pressed = capture_photo
 
 # Keep the script running and listening for events
 pause()
