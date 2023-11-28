40fd7638e9bbab485e295b5af5febbec60ca513b
domm <dommferger@gmail.com>
2023-11-28 12:56:43 -0500

    homography works?

--- img_align.py
+++ img_align.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
+import os
+
+# Set the output directory for aligned images and the GIF
+OUTPUT_DIR = os.path.expanduser("~/Desktop/riibiit/PICTURES")
 
 # Load the images
-image1 = cv2.imread('path/to/image1.jpg', cv2.IMREAD_GRAYSCALE)
-image2 = cv2.imread('path/to/image2.jpg', cv2.IMREAD_GRAYSCALE)  # The baseline image
-image3 = cv2.imread('path/to/image3.jpg', cv2.IMREAD_GRAYSCALE)
-image4 = cv2.imread('path/to/image4.jpg', cv2.IMREAD_GRAYSCALE)
+image1 = cv2.imread(os.path.join(OUTPUT_DIR, 'image1.jpg'), cv2.IMREAD_GRAYSCALE)
+image2 = cv2.imread(os.path.join(OUTPUT_DIR, 'image2.jpg'), cv2.IMREAD_GRAYSCALE)  # The baseline image
+image3 = cv2.imread(os.path.join(OUTPUT_DIR, 'image3.jpg'), cv2.IMREAD_GRAYSCALE)
+image4 = cv2.imread(os.path.join(OUTPUT_DIR, 'image4.jpg'), cv2.IMREAD_GRAYSCALE)
 
 # Initialize ORB detector
 orb = cv2.ORB_create()
@@ -61,6 +65,23 @@ else:
     aligned_image1 = cv2.warpPerspective(image1, homography_matrix1, (image2.shape[1], image2.shape[0]))
     aligned_image3 = cv2.warpPerspective(image3, homography_matrix3, (image2.shape[1], image2.shape[0]))
     aligned_image4 = cv2.warpPerspective(image4, homography_matrix4, (image2.shape[1], image2.shape[0]))
+
+    # Save aligned images
+    cv2.imwrite(os.path.join(OUTPUT_DIR, 'Align1.jpg'), aligned_image1)
+    cv2.imwrite(os.path.join(OUTPUT_DIR, 'Align3.jpg'), aligned_image3)
+    cv2.imwrite(os.path.join(OUTPUT_DIR, 'Align4.jpg'), aligned_image4)
+
+    # Create a looping GIF
+    aligned_image_paths = [os.path.join(OUTPUT_DIR, f'Align{i}.jpg') for i in [1, 3, 4, 3]]
+    gif_path = os.path.join(OUTPUT_DIR, 'aligned_images.gif')
+
+    images = [cv2.imread(path) for path in aligned_image_paths]
+    gif_images = images + images[::-1]  # Reverse the order for the looping effect
+
+    cv2.imwrite(gif_path, gif_images[0], [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    with cv2.VideoWriter(gif_path, cv2.VideoWriter_fourcc(*"MJPG"), 1, (images[0].shape[1], images[0].shape[0])) as video:
+        for img in gif_images:
+            video.write(img)
 
     # Display aligned images
     cv2.imshow('Aligned image1', aligned_image1)
